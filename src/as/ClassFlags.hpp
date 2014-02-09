2ad80b20edb3ab30974848a277f541d515235862
Chase W <chasew@silkweaver.com>
2014-02-09 15:30:46 -0500

    Fixed for compiling with AS 2.28.1 + MinGW 4.8.1

--- src/as/ClassFlags.hpp
+++ src/as/ClassFlags.hpp
@@ -6,37 +6,15 @@
 
 namespace as
 {
-	// Apparentally I don't actually need this... I'll it here just in case
 	template< class CLASS >
 	int getClassFlags()
 	{
 		int ret = asOBJ_APP_CLASS;
 		
-		// Apparentally these are from an older version of the standard?
-		// I'll put the proper once I upgrade to some version where GCC
-		// has the proper ones :P
+		// TODO: Update when I get a compiler that supports the right ones...
+		
+		//if ( !std::is_trivially_default_constructible< CLASS >::value )
 		if ( !std::has_trivial_default_constructor< CLASS >::value )
-		{
-			ret |= asOBJ_APP_CLASS_CONSTRUCTOR;
-		}
-		
-		if ( !std::has_trivial_destructor< CLASS >::value )
-		{
-			ret |= asOBJ_APP_CLASS_DESTRUCTOR;
-		}
-		
-		if ( !std::has_trivial_assign< CLASS >::value )
-		{
-			ret |= asOBJ_APP_CLASS_ASSIGNMENT;
-		}
-		
-		if ( !std::has_trivial_copy_constructor< CLASS >::value )
-		{
-			ret |= asOBJ_APP_CLASS_COPY_CONSTRUCTOR;
-		}
-		
-		/*
-		if ( !std::is_trivially_default_constructible< CLASS >::value )
 		{
 			ret |= asOBJ_APP_CLASS_CONSTRUCTOR;
 		}
@@ -46,16 +24,16 @@ namespace as
 			ret |= asOBJ_APP_CLASS_DESTRUCTOR;
 		}
 		
-		if ( !std::is_trivially_copy_assignable< CLASS >::value )
+		//if ( !std::is_trivially_copy_assignable< CLASS >::value )
+		if ( !std::has_trivial_copy_assign< CLASS >::value )
 		{
 			ret |= asOBJ_APP_CLASS_ASSIGNMENT;
 		}
 		
-		if ( !std::is_trivially_copy_constructible< CLASS >::value )
+		if ( !std::has_trivial_copy_constructor< CLASS >::value )
 		{
-			ret |= asOBJ_APP_COPY_CONSTRUCTOR;
+			ret |= asOBJ_APP_CLASS_COPY_CONSTRUCTOR;
 		}
-		*/
 		
 		return ret;
 	}
